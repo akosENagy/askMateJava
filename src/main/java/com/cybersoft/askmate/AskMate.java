652e9295daafd4bdb7f7a92b1dc6200552bfa815
akosENagy <akos.e.nagy@gmail.com>
2017-11-13 14:58:20 +0100

    Fixed question getall query, removed dummy data populate function

--- src/main/java/com/cybersoft/askmate/AskMate.java
+++ src/main/java/com/cybersoft/askmate/AskMate.java
@@ -1,7 +1,6 @@
 package com.cybersoft.askmate;
 
 import com.cybersoft.askmate.controller.Controller;
-import com.cybersoft.askmate.model.Question;
 
 import static spark.Spark.*;
 
@@ -15,16 +14,9 @@ public class AskMate {
         staticFileLocation("/public");
         port(8888);
 
-        // data business
-        populateQuestionDao();
 
         // routes
         get("/", Controller::renderQuestions);
         post("/submit-new-question", Controller::submitQuestion);
-    }
-
-    private static void populateQuestionDao() {
-        new Question("Why is it that a snorlax is blocking my way?", "I really can't get past this fat fuck. I know i need a pokéflute but where do I find one?");
-        new Question("Orly?", "O R L Y ?");
     }
 }
--- src/main/java/com/cybersoft/askmate/model/Question.java
+++ src/main/java/com/cybersoft/askmate/model/Question.java
@@ -8,7 +8,7 @@ import java.sql.Timestamp;
 
 @NamedQuery(
         name = "Question.getAll",
-        query = "SELECT q FROM Question q"
+        query = "SELECT q FROM Question q ORDER BY timestamp DESC"
 )
 @Entity
 @Table(name = "Question")
@@ -42,4 +42,39 @@ public class Question {
 //    public void addAnswer(Answer answer) {
 //        this.answerList.add(answer);
 //    }
+
+
+    // Getters & Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
 }
