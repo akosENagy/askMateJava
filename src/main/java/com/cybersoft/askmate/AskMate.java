123a6b2c8895071d5c3e0c55cf3406f3041f7a7b
akosENagy <akos.e.nagy@gmail.com>
2017-11-28 11:20:26 +0100

    Asking now requires a user login, and controller no longer uses static methods.

--- src/main/java/com/cybersoft/askmate/AskMate.java
+++ src/main/java/com/cybersoft/askmate/AskMate.java
@@ -7,19 +7,26 @@ import static spark.Spark.*;
 
 public class AskMate {
 
-    public static void main(String[] args) {
+    private Controller controller;
 
+    public static void main(String[] args) {
+        AskMate app = new AskMate();
+        app.start();
+    }
+
+    private void start() {
         // server settings
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
         port(8888);
 
+        this.controller = new Controller();
 
         // routes
-        get("/", Controller::renderQuestions);
-        get("/register", Controller::renderRegsiterForm);
-        get("/questions/:id", Controller::renderSingleQuestion);
-        post("/submit-new-question", Controller::submitQuestion);
-        post("/questions/:id/answer", Controller::submitAnswer);
+        get("/", controller::renderQuestions);
+        get("/register", controller::renderRegsiterForm);
+        get("/questions/:id", controller::renderSingleQuestion);
+        post("/submit-new-question", controller::submitQuestion);
+        post("/questions/:id/answer", controller::submitAnswer);
     }
 }
--- src/main/java/com/cybersoft/askmate/controller/Controller.java
+++ src/main/java/com/cybersoft/askmate/controller/Controller.java
@@ -16,15 +16,17 @@ import java.util.Map;
 
 public class Controller {
 
+    private Map params = new HashMap();
+
     // Rendering functions
-    public static String renderQuestions(Request req, Response res){
+    public String renderQuestions(Request req, Response res){
         Map params = new HashMap<>();
         List<Question> questions = DataManager.getInstance().getEntityManager().createNamedQuery("Question.getAll").getResultList();
         params.put("questions", questions);
         return renderTemplate(params, "index");
     }
 
-    public static String renderSingleQuestion(Request req, Response res) {
+    public String renderSingleQuestion(Request req, Response res) {
         Map params = new HashMap<>();
         Query query = DataManager.getInstance().getEntityManager().createNamedQuery("Question.getById");
         String path = req.pathInfo();
@@ -36,20 +38,20 @@ public class Controller {
         return renderTemplate(params, "details");
     }
 
-    public static String renderRegsiterForm(Request request, Response response) {
+    public String renderRegsiterForm(Request request, Response response) {
         return renderTemplate(new HashMap(), "register");
     }
 
     //Utilities
 
-    public static String submitQuestion(Request req, Response res) {
+    public String submitQuestion(Request req, Response res) {
         Question question = createQuestionFromRequest(req);
         DataManager.getInstance().persist(question);
         res.redirect("/");
         return null;
     }
 
-    public static String submitAnswer(Request req, Response res) {
+    public String submitAnswer(Request req, Response res) {
         Answer answer =  new Answer(req.queryParams("new-answer-title"), req.queryParams("new-answer-content"));
         Query query = DataManager.getInstance().getEntityManager().createNamedQuery("Question.getById");
         String path = req.pathInfo();
@@ -64,11 +66,11 @@ public class Controller {
         return null;
     }
 
-    private static String renderTemplate(Map model, String template) {
+    private String renderTemplate(Map model, String template) {
         return new ThymeleafTemplateEngine().render(new ModelAndView(model, template));
     }
 
-    public static Question createQuestionFromRequest(Request req) {
+    private Question createQuestionFromRequest(Request req) {
         return new Question(req.queryParams("new-question-title"), req.queryParams("new-question-content"));
     }
 }
